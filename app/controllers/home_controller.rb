45e6022a728324216e7e2b939e119c1cf492b5f0
Francisco <franjfloresa@gmail.com>
2017-11-20 00:37:35 +0000

    Fixed pagination issues.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	@jerseys = Jersey.all.page params[:page]
-
-
+  	@jerseys = Jersey.all.page(params[:page])
   end 
-
-  def search
-  	 #  	 @js = @jerseys = Jersey.search(params[:search])
-    # @jsearch = Jersey.search(params[:search])
-     @js = Jersey.search(params[:search])
-    # @j_counts = Jersey.count(:group => :player[:name], 
-    #    :order => 'count_all DESC', :limit => 100)
-  end
 end
