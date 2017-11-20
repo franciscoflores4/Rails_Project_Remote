74cf9b39b2ff49139f824ced4fd2e3ed9670f93b
Francisco <franjfloresa@gmail.com>
2017-11-20 04:35:29 +0000

    Added different methods in the cart controller.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,21 @@
 class CartController < ApplicationController
+	def index
+		total
+	end
+
+	def cart
+		session[:cart] << params[:jersey] unless session[:cart].include? params[:jersey]
+	end
+
+	def remove
+		@cart_item = params[:jersey]
+		session[:cart].delete @cart_item
+		total
+	end
+
+	private 
+	def total
+		@jerseys = Jersey.where(slug: session[:cart])
+		@sub_total - @jerseys.map(&:price).reduce(:+)
+	end
 end
